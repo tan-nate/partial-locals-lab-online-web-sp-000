c56b39b7aad5d222b45fe6f36c464c2c52248c56
Nate Tan <nktan93@gmail.com>
2019-07-29 18:04:34 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,16 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(criteria)
+    capitalized_criteria = criteria.capitalize
+    names = self.all.collect {|student| student.name}
+    results_as_strings = names.grep(/#{capitalized_criteria}\w+/)
+    #binding.pry
+    results_as_objects = []
+    results_as_strings.each do |name|
+      results_as_objects << self.all.find_by(name: name)
+    end
+    results_as_objects
+  end
 end
