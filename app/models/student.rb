87aa3ac325ae0b7cb8c77b9472eeb9cf3e2fe60c
Nate Tan <nktan93@gmail.com>
2019-07-29 17:16:44 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -18,11 +18,10 @@ class Student < ActiveRecord::Base
     capitalized_criteria = criteria.capitalize
     names = self.all.collect {|student| student.name}
     results_as_strings = names.grep(/#{capitalized_criteria}\w+/)
-    binding.pry
-    results_as_objects = self.all.select do |student|
-      results_as_strings.each do |result|
-        student.name == result
-      end
+    #binding.pry
+    results_as_objects = []
+    results_as_strings.each do |name|
+      results_as_objects << self.all.find_by(name: name)
     end
   end
 end
